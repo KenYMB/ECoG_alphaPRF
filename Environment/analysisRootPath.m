a213e46ac1259cd93d72576c4bc5cca41c940edb
KenYMB <yuasa.research@gmail.com>
2023-07-12 12:10:46 -0400

    update root paths

--- Environment/analysisRootPath.m
+++ Environment/analysisRootPath.m
@@ -6,13 +6,14 @@ function rootPath = analysisRootPath()
 %-- Set your root path for analysis
 rootPath = '';
 
-%-- When you copy analysisRootPath to working space, then please comment out next line.
-rootPath = regexprep(mfilename('fullpath'),mfilename,'');
-
-%-- Set toolbox root directory if rootPath does not exist (default)
+%-- Set current directory as the root path if rootPath does not exist and
+%-- if this function is placed on the working directory,  
+%-- or set the toolbox root directory as the root path (default)
 if ~exist(rootPath,'dir')
-    rootPath = fullfile(fileparts(mfilename('fullpath')),'..','..');
-    [st,fileinfo] = fileattrib(rootPath);
+    rootPath = fileparts(mfilename('fullpath'));
+    if ~isequal(rootPath,pwd)
+    rootPath = fullfile(rootPath,'..');
+    end
+    [~,fileinfo] = fileattrib(rootPath);
     rootPath = fileinfo.Name;
 end
-
--- Environment/bidsRootPath.m
+++ Environment/bidsRootPath.m
@@ -1,8 +1,14 @@
 function rootPath = bidsRootPath()
 
-% Users of the temporalECoG repository should update this path to point to
+% Users of the ECoG_alpha toolbox should update this path to point to
 % where they downloaded the BAIR dataset.
 
-rootPath = '/Volumes/server/Projects/BAIR/Data/BIDS/visual_ecog_recoded';
+%-- Set your root path to download dataset
+rootPath = '';
+
+%-- Set the root path as 'analysisRootPath/BIDS' if rootPath does not exist (default)
+if ~exist(rootPath,'dir')
+    rootPath = fullfile(analysisRootPath, 'BIDS'); 
+end
 
 end
