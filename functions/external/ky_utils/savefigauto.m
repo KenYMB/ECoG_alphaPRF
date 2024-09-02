5a1314b11308d7626877857c7659a99c9a41def9
KenYMB <yuasa.research@gmail.com>
2024-09-02 05:08:39 -0400

    update savefigauto

--- functions/external/ky_utils/savefigauto.m
+++ functions/external/ky_utils/savefigauto.m
@@ -11,10 +11,12 @@ function savefigauto(fig, filename, varargin)
 %     This option is same as {'Render','painters'}
 %   any options compatible with SAVEAS or HGEXPORT can be used.
 % 
-% see also, saveas, hgexport
+% see also, saveas, hgexport, exportgraphics
 
 % 20210623 Yuasa
 % 20220621 Yuasa - add '-vector' option
+% 20240527 Yuasa - update to use exportgraphics in case hgexport is out-of-date
+% 20240901 Yuasa - suppress warning of hgexport
 
 %-- parameter
 narginchk(2,inf);
@@ -35,21 +37,44 @@ if ~iscell(formats)
     formats = {formats};
 end
 
+%-- suppress warning of hgexport
+warnid = 'MATLAB:hgexport:StyleSheetNotSupportedInFutureRelease';
+warnst = warning('query',warnid);
+
 %-- save
-for fmt = reshape(formats,1,[])
+try
+ for fmt = reshape(formats,1,[])
     switch fmt{:}
         case {'fig','m','mfig'}
             saveas(fig, filename, fmt{:});
         case {'pdf','eps','epsc','eps2','epsc2','meta','svg','ps','psc','ps2','psc2'}
-          if isvec
-            hgexport(fig, filename, hgexport('factorystyle'), ...
-                'Renderer', 'painters', 'Format', fmt{:}, varargin{:});
+          if exist('hgexport','file')
+            if isvec
+              hgexport(fig, filename, hgexport('factorystyle'), ...
+                  'Renderer', 'painters', 'Format', fmt{:}, varargin{:});
+            else
+              hgexport(fig, filename, hgexport('factorystyle'), ...
+                  'Format', fmt{:}, varargin{:});
+            end
           else
-            hgexport(fig, filename, hgexport('factorystyle'), ...
-                'Format', fmt{:}, varargin{:});
+            if isvec
+              exportgraphics(fig, sprintf('%s.%s',filename, fmt{:}),...
+                  'ContentType', 'vector', varargin{:});
+            else
+              exportgraphics(fig, sprintf('%s.%s',filename, fmt{:}), varargin{:});
+            end
           end
         otherwise
+          if exist('hgexport','file')
             hgexport(fig, filename, hgexport('factorystyle'), ...
                 'Format', fmt{:}, varargin{:});
+          else
+            exportgraphics(fig, sprintf('%s.%s',filename, fmt{:}), varargin{:});
+          end
     end
+ end
+ warning(warnst);
+catch ME
+ warning(warnst);
+ rethrow(ME);
 end
